caff0f4bd40ac459b3872a392cdb22ca090e1772
Shaun Hudson <76202749+Istoc42@users.noreply.github.com>
2021-10-14 13:17:08 +0100

    Version 1

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Grab DOM elements
+
+// Get search bar
+const search = document.getElementById('search')
+
+// Get dropdown output
+const matchList = document.getElementById('match-list')
+
+// Create a function to search the JSON and filter it
+const searchCodes = async (searchText) => {
+  const res = await fetch('../data/codes.json')
+  const snomeds = await res.json()
+
+  // Get matches to current text input
+  let matches = snomeds.filter((snomed) => {
+    const regex = new RegExp(`^${searchText}`, 'gi')
+    return snomed.name.match(regex) || snomed.code.match(regex)
+  })
+
+  // Conditional to clear
+  if (searchText.length === 0) {
+    matches = []
+  }
+
+  outputHtml(matches)
+}
+
+// Show results in HTML
+const outputHtml = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+      <div class="card card-body mb-1">
+        <h4>${match.code}</h4>
+        <small>${match.name}</small>
+      </div>
+    `
+      )
+      .join('')
+
+    matchList.innerHTML = html
+  }
+}
+
+// Detect input in the search bar
+search.addEventListener('input', () => searchCodes(search.value))
